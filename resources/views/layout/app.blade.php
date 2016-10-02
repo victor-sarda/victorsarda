48e02ce72582d3d2dedb49d8abf8a871a04d142f
Victor <victor.sarda@icloud.com>
2016-10-02 19:28:05 +0200

    Created app base page layout

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Victor Sarda | @yield('title')</title>
+
+    <meta name="Description" content="@yield('description')" />
+    <meta name="author" content="Victor Sarda">
+    <meta name="keywords" content="victor, sarda, victorsarda, web, developpement, development, html, php, site, contact, freelance">
+
+    <!-- Bootstrap -->
+    <link href="css/app.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+@yield('content')
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/app.js"></script>
+</body>
+</html>
